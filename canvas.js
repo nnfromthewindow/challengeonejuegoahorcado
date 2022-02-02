0694ff2f21fdf9db641b7785456b54be4ccac7d3
Nicolas Nuccelli <nuccelli@hotmail.com>
2022-02-02 00:32:14 -0300

    Iniciamos proyecto, horca dibujada

---
+++ canvas.js
@@ -0,0 +1,46 @@
+var canvas = document.getElementById("ahorcado");
+var pincel = canvas.getContext("2d");
+
+function drawHang() {
+  let ancholinea = 7;
+
+  function drawBase() {
+    pincel.beginPath();
+    pincel.moveTo(50, 600);
+    pincel.lineTo(350, 600);
+    pincel.lineTo(200, 550);
+    pincel.closePath();
+    pincel.fillStyle = "black";
+    pincel.fill();
+  }
+  drawBase();
+
+  pincel.beginPath();
+  pincel.strokeStyle = "black";
+  pincel.moveTo(200, 560);
+  pincel.lineTo(200, 100);
+  pincel.lineTo(500, 100);
+  pincel.lineTo(500, 150);
+  pincel.lineWidth = ancholinea;
+  pincel.stroke();
+}
+
+function head() {}
+
+function body() {}
+
+function legLeft() {}
+
+function legRight() {}
+
+function armLeft() {}
+
+function armRight() {}
+
+drawHang();
+head();
+body();
+legLeft();
+legRight();
+armLeft();
+armRight();
