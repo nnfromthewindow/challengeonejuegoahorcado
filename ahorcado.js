610185069e2bf3da82616c5c55545242fab6081f
Nicolas Nuccelli <nuccelli@hotmail.com>
2022-02-04 00:17:07 -0300

    mejorando estilo a porcentajes

--- ahorcado.js
+++ ahorcado.js
@@ -5,3 +5,12 @@ legLeft();
 legRight();
 armLeft();
 armRight();
+
+let wordList = { JUAN, PERRO, CARTA };
+let fails = {};
+let selectedWord = wordList.indexOf(2);
+console.log(selectedWord);
+
+function initGame() {}
+
+function addWord() {}
